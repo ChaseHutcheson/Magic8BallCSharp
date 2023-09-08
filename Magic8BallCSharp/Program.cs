901fdb1dcbe80e37cbbad6142caf9165cc479af7
Chase Hutcheson <111781666+ChaseHutcheson@users.noreply.github.com>
2023-09-08 13:18:03 -0400

    Add project files.

---
+++ Magic8BallCSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magic8BallCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public Magic8Ball()
+        {
+            Random random = new Random();
+
+            switch (random)
+            {
+                case 1:
+                    string answer = "It is certain";
+                    return answer;
+                    break;
+            }
+
+        }
+    }
+}
